c49fc53c1e41c0fb787d36f6a13f7c6b91a79142
lhhformac <384234884@qq.com>
2019-04-09 18:05:51 +0800

    lhh_bootstrap

---
+++ app/src/components/index/codeList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="quant-index-page">
+    <div class="quant-index-wrap">
+      <div class="quant-index-box">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quantIndexPage',
+  data () {
+    return {
+      msg: 'quantIndexPage'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ app/src/view/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="quant-index-page">
+    <div class="quant-index-wrap">
+      <div class="quant-index-box">
+        <div class="code-list">
+          bootstrap
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quantIndexPage',
+  data () {
+    return {
+      msg: 'quantIndexPage'
+    }
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
